37578c78881e74b1a73525d4064dd8f2b0d97d0e
Francis <brakay1136@gmail.com>
2025-09-24 09:31:48 -0700

    game class created

---
+++ app.js
@@ -0,0 +1,13 @@
+class MemoryGame {
+  constructor() {
+    this.scoreCard = document.querySelector(".score");
+    this.reset = document.querySelector(".reset-btn");
+    this.score = 0;
+  }
+
+  gameMechanics() {}
+  addEventListeners() {}
+}
+
+const memoryGame = new MemoryGame();
+memoryGame.addEventListeners();
