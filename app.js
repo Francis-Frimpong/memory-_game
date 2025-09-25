49ab35f1a4eb319e454710ccf907c0a7381b96af
Francis <brakay1136@gmail.com>
2025-09-25 10:35:35 -0700

    card flipping function done

--- app.js
+++ app.js
@@ -5,11 +5,14 @@ class MemoryGame {
     this.gameBoard = document.querySelector(".board");
     this.checkBox = document.querySelectorAll("input");
     this.score = 0;
+    this.firstPick = null;
   }
 
   gameMechanics(card) {
     // flip card
     card.classList.toggle("flipped", card.checked);
+
+    // compare click card
   }
 
   shuffleCard(array) {
@@ -22,13 +25,40 @@ class MemoryGame {
       document.querySelector(".board").appendChild(card);
     }
   }
+
   addEventListeners() {
     this.reset.addEventListener("click", () =>
       this.shuffleCard(Array.from(this.gameBoard.children))
     );
 
     this.checkBox.forEach((card) => {
-      card.addEventListener("change", () => this.gameMechanics(card));
+      card.addEventListener("change", (e) => {
+        this.gameMechanics(card);
+        const targetCard = e.target.closest(".card").dataset.card;
+
+        if (this.firstPick === null) {
+          this.firstPick = { element: card, value: targetCard };
+        } else {
+          if (this.firstPick.value === targetCard) {
+            console.log("Match");
+            this.score++;
+            this.scoreCard.textContent = this.score;
+          } else {
+            console.log("Not a match");
+
+            // keep a local copy of the first card before nulling it
+            const firstPickElement = this.firstPick.element;
+
+            setTimeout(() => {
+              firstPickElement.classList.remove("flipped");
+              firstPickElement.checked = false;
+              card.classList.remove("flipped");
+              card.checked = false;
+            }, 1500);
+          }
+          this.firstPick = null;
+        }
+      });
     });
   }
 }
